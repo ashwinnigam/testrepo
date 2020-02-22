6951c8da8624755acd55daca3d547135100313b7
ashwin_nigam <ashwin.nigam91@gmail.com>
2020-02-22 16:30:15 +0530

    Initial commit

---
+++ src/main/java/com/yts/art/Art23Application.java
@@ -0,0 +1,13 @@
+package com.yts.art;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Art23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Art23Application.class, args);
+	}
+
+}
